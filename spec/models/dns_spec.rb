808e32989b207ccb387e31b100de11a89fbc0b50
isaac israel <email.iisrael@gmail.com>
2019-06-11 21:53:29 -0300

    Define Dns test cases

--- spec/models/dns_spec.rb
+++ spec/models/dns_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Dns, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be valid with a IP'
+  it 'is invalid without IP'
+  it 'is invalid duplicate IP'
+  it 'is invalid if is not IPv4 format'
 end
