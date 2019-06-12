c5edc314064f823a527fd1b404fa8954f896ff63
isaac israel <email.iisrael@gmail.com>
2019-06-11 21:56:30 -0300

    Create Dns model test

--- spec/models/dns_spec.rb
+++ spec/models/dns_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Dns, type: :model do
-  it 'should be valid with a IP'
-  it 'is invalid without IP'
-  it 'is invalid duplicate IP'
-  it 'is invalid if is not IPv4 format'
+  it 'should be valid with a IP' do
+    dns = Dns.new(IP: '1.1.1.1')
+    expect(dns).to be_valid
+  end
+  it 'is invalid without IP' do
+    dns = FactoryBot.build(:dns, IP: nil)
+    dns.valid?
+    expect(dns.errors[:IP]).to include("can't be blank")
+  end
+  it 'is invalid duplicate IP' do
+    FactoryBot.create(:dns, IP: '1.1.1.1')
+    dns = FactoryBot.build(:dns, IP: '1.1.1.1')
+    dns.valid?
+    expect(dns.errors[:IP]).to include('has already been taken')
+  end
+  it 'is invalid if is not IPv4 format' do
+    dns = FactoryBot.build(:dns, IP: '1000000.1000000.1000000.100000')
+    dns.valid?
+    expect(dns.errors[:IP]).to include('is not IPv4 format')
+  end
 end
