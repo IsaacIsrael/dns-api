79636cd0b71a252e9bc4bb337cefdd3b07a97fd2
isaac israel <email.iisrael@gmail.com>
2019-06-12 10:39:01 -0300

    Validate test when without params and when given filter

--- app/interactors/filter_dns_hostnames_per_page.rb
+++ app/interactors/filter_dns_hostnames_per_page.rb
@@ -2,5 +2,12 @@ class FilterDnsHostnamesPerPage
   include Interactor
 
   def call
+    context.limit = context.limit || 5
+    context.total = context.dns.count
+    context.dns = context.dns.page(context.page).per(context.limit)
+    context.hostnames = context.hostnames.where(id: Hostname
+                                                      .joins(:dns)
+                                                      .where('dns.id IN (?)', context.dns.select('id'))
+                                                      .select('id'))
   end
 end
