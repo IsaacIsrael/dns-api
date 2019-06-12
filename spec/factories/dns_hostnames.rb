3b02cf6ba62c9f0fcf4abcc4fcfa19f08719587a
isaac israel <email.iisrael@gmail.com>
2019-06-11 22:09:21 -0300

    Remove dns_hostname test

--- spec/factories/dns_hostnames.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :dns_hostname do
-    dns nil
-    hostname nil
-  end
-end
--- spec/models/dns_hostname_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe DnsHostname, type: :model do
-end
