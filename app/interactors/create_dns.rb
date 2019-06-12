e30edba7e56d977f8424fec35f93633464a0a342
isaac israel <email.iisrael@gmail.com>
2019-06-11 22:13:40 -0300

    Run rails generate interactor create_dns

---
+++ app/interactors/create_dns.rb
@@ -0,0 +1,7 @@
+class CreateDns
+  include Interactor
+
+  def call
+    # TODO
+  end
+end
---
+++ spec/interactors/create_dns_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe CreateDns, type: :interactor do
+  describe '.call' do
+  end
+end
