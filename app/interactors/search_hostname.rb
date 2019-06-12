8e903e813629d38242365bca197810a63c93361b
isaac israel <email.iisrael@gmail.com>
2019-06-12 10:24:27 -0300

    Validate test when given excluded filter

--- app/interactors/search_hostname.rb
+++ app/interactors/search_hostname.rb
@@ -2,10 +2,11 @@ class SearchHostname
   include Interactor
 
   def call
-     context.hostnames = Hostname.joins(:dns)
-                        .where('dns.id IN (?)', context.dns.select('id'))
-                        .group('hostnames.id, name')
-                        .where.not(name: context.filter&.[](:included))
-                        .select('hostnames.id ,name , COUNT(name) as match')
+    filter = (context.filter&.[](:included) || []) + (context.filter&.[](:excluded) || [])
+    context.hostnames = Hostname.joins(:dns)
+                                .where('dns.id IN (?)', context.dns.select('id'))
+                                .group('hostnames.id, name')
+                                .where.not(name: filter)
+                                .select('hostnames.id ,name , COUNT(name) as match')
   end
 end
