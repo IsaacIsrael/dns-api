a813ca7d5a3bd434a6bfc2e99fbfbd69b930a4f6
isaac israel <email.iisrael@gmail.com>
2019-06-11 22:05:42 -0300

    Create Hostname model test

--- spec/models/hostname_spec.rb
+++ spec/models/hostname_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Hostname, type: :model do
-  it 'should be valid with a name'
-  it 'is invalid without name'
-  it 'is invalid duplicate name'
+  it 'should be valid with a name' do
+    hostname = Hostname.new(name: 'ipsum.com')
+    expect(hostname).to be_valid
+  end
+  it 'is invalid without name' do
+    hostname = FactoryBot.build(:hostname, name: nil)
+    hostname.valid?
+    expect(hostname.errors[:name]).to include("can't be blank")
+  end
+  it 'is invalid duplicate name' do
+    FactoryBot.create(:hostname, name: 'ipsum.com')
+    hostname = FactoryBot.build(:hostname, name: 'ipsum.com')
+    hostname.valid?
+    expect(hostname.errors[:name]).to include('has already been taken')
+  end
 end
