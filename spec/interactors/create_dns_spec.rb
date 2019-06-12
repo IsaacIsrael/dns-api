62a2c74e71e5c6c1b5c82c711203bfe901527ee3
isaac israel <email.iisrael@gmail.com>
2019-06-11 22:23:41 -0300

    Define CreateDns test cases

--- spec/interactors/create_dns_spec.rb
+++ spec/interactors/create_dns_spec.rb
@@ -2,5 +2,15 @@ require 'rails_helper'
 
 RSpec.describe CreateDns, type: :interactor do
   describe '.call' do
+    context 'when given valid dns' do
+      let(:params) { FactoryBot.attributes_for(:dns) }
+      subject(:context) { CreateDns.call(params) }
+      it 'succeeds'
+      it 'save dns'
+    end
+    context 'when given invalid dns' do
+      it 'fails'
+      it 'dont save dns'
+    end
   end
 end
