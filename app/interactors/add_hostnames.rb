274cf8b60b6266d9d875bcba9df4e183b80b2f04
isaac israel <email.iisrael@gmail.com>
2019-06-11 22:57:07 -0300

    Validate test  when given invalid hostname

--- app/interactors/add_hostnames.rb
+++ app/interactors/add_hostnames.rb
@@ -2,8 +2,13 @@ class AddHostnames
   include Interactor
 
   def call
-    context.hostnames.each do |hostname|
-      context.dns.hostnames << Hostname.find_or_create_by(name: hostname)
+    ActiveRecord::Base.transaction do
+      context.hostnames.each do |hostname|
+        context.dns.hostnames << Hostname.find_or_create_by(name: hostname)
+      end
+    rescue ActiveRecord::RecordInvalid => exception
+      context.fail!(errors: exception.message)
+      raise ActiveRecord::Rollback
     end
   end
 end
