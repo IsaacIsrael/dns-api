47666cb47a040fe0719c73c85bdb7d4eeae5d949
isaac israel <email.iisrael@gmail.com>
2019-06-12 10:51:31 -0300

    Create SearchDnsHostnamesAsJson interactor test

--- spec/interactors/search_dns_hostnames_as_json_spec.rb
+++ spec/interactors/search_dns_hostnames_as_json_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe SearchDnsHostnamesAsJson, type: :interactor do
-  let!(:dns1) {  CreateDnsWithHostname.call(ip: '1.1.1.1', hostnames: ['lorem.com', 'ipsum.com', 'dolor.com', 'amet.com']) }
-  let!(:dns2) {  CreateDnsWithHostname.call(ip: '2.2.2.2', hostnames: ['ipsum.com']) }
-  let!(:dns3) {  CreateDnsWithHostname.call(ip: '3.3.3.3', hostnames: ['dolor.com', 'ipsum.com', 'amet.com']) }
-  let!(:dns4) {  CreateDnsWithHostname.call(ip: '4.4.4.4', hostnames: ['dolor.com', 'ipsum.com', 'sit.com', 'amet.com']) }
-  let!(:dns5) {  CreateDnsWithHostname.call(ip: '5.5.5.5', hostnames: ['dolor.com', 'sit.com']) }
+  let!(:dns1) {  CreateDnsWithHostnames.call(ip: '1.1.1.1', hostnames: ['lorem.com', 'ipsum.com', 'dolor.com', 'amet.com']) }
+  let!(:dns2) {  CreateDnsWithHostnames.call(ip: '2.2.2.2', hostnames: ['ipsum.com']) }
+  let!(:dns3) {  CreateDnsWithHostnames.call(ip: '3.3.3.3', hostnames: ['dolor.com', 'ipsum.com', 'amet.com']) }
+  let!(:dns4) {  CreateDnsWithHostnames.call(ip: '4.4.4.4', hostnames: ['dolor.com', 'ipsum.com', 'sit.com', 'amet.com']) }
+  let!(:dns5) {  CreateDnsWithHostnames.call(ip: '5.5.5.5', hostnames: ['dolor.com', 'sit.com']) }
 
   describe '.call' do
     let(:filter) { { included: ['ipsum.com', 'dolor.com'], excluded: ['sit.com'] }}
@@ -14,9 +14,17 @@ RSpec.describe SearchDnsHostnamesAsJson, type: :interactor do
     let(:per_page) {FilterDnsHostnamesPerPage.call(page: 2, limit: 1, dns: dns, hostnames: hostnames) }
     subject(:context) {SearchDnsHostnamesAsJson.call(total: per_page.total, dns: per_page.dns, hostnames: per_page.hostnames) }
 
-    it 'succeeds'
-    it 'return total'
-    it 'return  dns'
-    it 'return  hostnames'
+    it 'succeeds' do
+      expect(context).to be_a_success
+    end
+    it 'return total' do
+      expect(context.response).to include('total')
+    end
+    it 'return  dns' do
+      expect(context.response).to include('dns')
+    end
+    it 'return  hostnames' do
+      expect(context.response).to include('hostnames')
+    end
   end
 end
