739f8a50f763e5e4517e063ce0160e61907e94c4
isaac israel <email.iisrael@gmail.com>
2019-06-11 23:28:31 -0300

    Create AP1/V1/dns POST request test

--- spec/requests/api_v1_dns_spec.rb
+++ spec/requests/api_v1_dns_spec.rb
@@ -3,22 +3,66 @@ require 'rails_helper'
 RSpec.describe "ApiV1Dns", type: :request do
   describe 'POST /api_v1_dns' do
     context 'when given valid params' do
-      it 'have http status 201'
-      it 'return dns created id'
-      it 'create a dns'
-      it 'create hostnames'
+      let(:ip) { FactoryBot.attributes_for(:dns)[:IP] }
+      let(:hostnames) { Array.new(5).map { FactoryBot.attributes_for(:hostname)[:name] } }
+      let(:dns_parameters) { { ip: ip, hostnames: hostnames } }
+      subject(:call) { post api_v1_dns_path, params: dns_parameters }
+
+      it 'have http status 201' do
+        call
+        expect(response).to have_http_status(201)
+      end
+      it 'return dns created id' do
+        call
+        expect(JSON.parse(response.body)).to include('id')
+      end
+      it 'create a dns' do
+        expect { call }.to change(Dns.all, :count).by(1)
+      end
+      it 'create hostnames' do
+        expect { call }.to change(Hostname.all, :count).by(5)
+      end
     end
     context 'when  not given dns' do
-      it 'have http status 400'
-      it 'return error'
-      it 'not create a dns'
-      it 'not create hostnames'
+      let(:hostnames) { Array.new(5).map { FactoryBot.attributes_for(:hostname)[:name] } }
+      let(:dns_parameters) { {  hostnames: hostnames } }
+      subject(:call) { post api_v1_dns_path, params: dns_parameters }
+
+      it 'have http status 400' do
+        call
+        expect(response).to have_http_status(400)
+      end
+      it 'return error' do
+        call
+        expect(JSON.parse(response.body)).to include('error')
+      end
+      it 'not create a dns' do
+        expect { call }.to_not change(Dns.all, :count)
+      end
+      it 'not create hostnames' do
+        expect { call }.to_not change(Hostname.all, :count)
+      end
     end
     context 'when given invalid hostnames' do
-      it 'have http status 400'
-      it 'return error'
-      it 'not create a dns'
-      it 'not create hostnames'
+      let(:ip) { FactoryBot.attributes_for(:dns)[:IP] }
+      let(:hostnames) { ['lorem.com', 'lorem.com'] }
+      let(:dns_parameters) { { ip: ip, hostnames: hostnames } }
+      subject(:call) { post api_v1_dns_path, params: dns_parameters }
+
+      it 'have http status 400' do
+        call
+        expect(response).to have_http_status(400)
+      end
+      it 'return error' do
+        call
+        expect(JSON.parse(response.body)).to include('error')
+      end
+      it 'not create a dns' do
+        expect { call }.to_not change(Dns.all, :count)
+      end
+      it 'not create hostnames' do
+        expect { call }.to_not change(Hostname.all, :count)
+      end
     end
   end
 end
