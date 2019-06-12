67cf4fe458d574acbcabe79051d1aca3c732d8d9
isaac israel <email.iisrael@gmail.com>
2019-06-11 23:12:04 -0300

    Define CreateDnsWithHostnames test cases

--- spec/interactors/create_dns_with_hostnames_spec.rb
+++ spec/interactors/create_dns_with_hostnames_spec.rb
@@ -2,5 +2,26 @@ require 'rails_helper'
 
 RSpec.describe CreateDnsWithHostnames, type: :interactor do
   describe '.call' do
+    context 'when given valid params' do
+      let(:params) { { ip: '1.1.1.1', hostnames: ['lorem.com', 'ipsum.com', 'amet.com'] } }
+      subject(:context) { CreateDnsWithHostname.call(params) }
+      it 'succeeds'
+      it 'save dns'
+      it 'save hostnames'
+    end
+    context 'when given invalid dns' do
+      let(:params) { { ip: nil, hostnames: ['lorem.com', 'ipsum.com', 'amet.com'] } }
+      subject(:context) { CreateDnsWithHostname.call(params) }
+      it 'fails'
+      it 'dont save dns'
+      it 'dont save hostnames'
+    end
+    context 'when given invalid hostname' do
+      let(:params) { { ip: '1.1.1.1', hostnames: ['lorem.com', 'lorem.com', 'amet.com'] } }
+      subject(:context) { CreateDnsWithHostname.call(params) }
+      it 'fails'
+      it 'dont save dns'
+      it 'dont save hostnames'
+    end
   end
 end
