d2c6c4add10f0a8d43a63068ee55a39feef3270b
isaac israel <email.iisrael@gmail.com>
2019-06-12 09:15:23 -0300

    Create SearchDns interactor test

--- spec/interactors/search_dns_spec.rb
+++ spec/interactors/search_dns_spec.rb
@@ -11,32 +11,54 @@ RSpec.describe SearchDns, type: :interactor do
     context 'when without params' do
       subject(:context) { SearchDns.call }
 
-      it 'succeeds'
-      it 'return dns array'
+      it 'succeeds' do
+        expect(context).to be_a_success
+      end
+      it 'return dns array ' do
+        expect(context.dns).to include(dns1.dns, dns2.dns, dns3.dns, dns4.dns, dns5.dns)
+      end
     end
     context 'when given included filter' do
       let(:filter) { { included:['ipsum.com', 'dolor.com'] } }
       subject(:context) { SearchDns.call(filter: filter) }
 
-      it 'succeeds'
-      it 'return dns array'
-      it 'not return other dns'
+      it 'succeeds' do
+        expect(context).to be_a_success
+      end
+      it 'return dns array ' do
+        expect(context.dns).to include(dns1.dns, dns3.dns, dns4.dns)
+      end
+      it 'not return other dns' do
+        expect(context.dns).to_not include(dns2.dns, dns5.dns)
+      end
     end
     context 'when given excluded filter' do
       let(:filter) { { excluded:['sit.com'] } }
       subject(:context) { SearchDns.call(filter: filter) }
 
-      it 'succeeds'
-      it 'return dns array'
-      it 'not return other dns'
+      it 'succeeds' do
+        expect(context).to be_a_success
+      end
+      it 'return dns array ' do
+        expect(context.dns).to include(dns1.dns, dns2.dns, dns3.dns)
+      end
+      it 'return dns array with the exect number of dns' do
+        expect(context.dns).to_not include(dns4.dns, dns5.dns)
+      end
     end
     context 'when given included and excluded filter' do
       let(:filter) { { included: ['ipsum.com', 'dolor.com'], excluded: ['sit.com'] } }
       subject(:context) { SearchDns.call(filter: filter) }
 
-      it 'succeeds'
-      it 'return dns array'
-      it 'not return other dns'
+      it 'succeeds' do
+        expect(context).to be_a_success
+      end
+      it 'return dns array ' do
+        expect(context.dns).to include(dns1.dns, dns3.dns)
+      end
+      it 'not return other dns' do
+        expect(context.dns).to_not include(dns4.dns, dns5.dns, dns2.dns)
+      end
     end
   end
 end
