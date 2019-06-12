0295fcfa6b242f4de507301c46e610d975f78e9c
isaac israel <email.iisrael@gmail.com>
2019-06-12 10:52:55 -0300

    Validate test SearchDnsHostnamesAsJson

--- app/interactors/search_dns_hostnames_as_json.rb
+++ app/interactors/search_dns_hostnames_as_json.rb
@@ -2,5 +2,9 @@ class SearchDnsHostnamesAsJson
   include Interactor
 
   def call
+    context.response = {}
+    context.response['total'] = context.total.as_json
+    context.response['dns'] = context.dns.as_json(only: [:id, :IP])
+    context.response['hostnames'] = context.hostnames.as_json(only: [:name, :match])
   end
 end
