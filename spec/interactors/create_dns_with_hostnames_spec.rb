b2007166046ed8890a4ae37fd0e003cb959320b2
isaac israel <email.iisrael@gmail.com>
2019-06-11 23:14:21 -0300

    Define CreateDnsWithHostnames test cases

--- spec/interactors/create_dns_with_hostnames_spec.rb
+++ spec/interactors/create_dns_with_hostnames_spec.rb
@@ -3,25 +3,43 @@ require 'rails_helper'
 RSpec.describe CreateDnsWithHostnames, type: :interactor do
   describe '.call' do
     context 'when given valid params' do
-      let(:params) { { ip: '1.1.1.1', hostnames: ['lorem.com', 'ipsum.com', 'amet.com'] } }
-      subject(:context) { CreateDnsWithHostname.call(params) }
-      it 'succeeds'
-      it 'save dns'
-      it 'save hostnames'
+      let(:params) { { IP: '1.1.1.1', hostnames: ['lorem.com', 'ipsum.com', 'amet.com'] } }
+      subject(:context) { CreateDnsWithHostnames.call(params) }
+      it 'succeeds' do
+        expect(context).to be_a_success
+      end
+      it 'save dns' do
+        expect { context }.to change(Dns.all, :count).by(1)
+      end
+      it 'save hostnames' do
+        expect { context }.to change(Hostname.all, :count).by(3)
+      end
     end
     context 'when given invalid dns' do
-      let(:params) { { ip: nil, hostnames: ['lorem.com', 'ipsum.com', 'amet.com'] } }
-      subject(:context) { CreateDnsWithHostname.call(params) }
-      it 'fails'
-      it 'dont save dns'
-      it 'dont save hostnames'
+      let(:params) { { IP: nil, hostnames: ['lorem.com', 'ipsum.com', 'amet.com'] } }
+      subject(:context) { CreateDnsWithHostnames.call(params) }
+      it 'fails' do
+        expect(context).to be_a_failure
+      end
+      it 'dont save dns' do
+        expect { context }.to_not change(Dns.all, :count)
+      end
+      it 'dont save hostnames' do
+        expect { context }.to_not change(Hostname.all, :count)
+      end
     end
     context 'when given invalid hostname' do
-      let(:params) { { ip: '1.1.1.1', hostnames: ['lorem.com', 'lorem.com', 'amet.com'] } }
-      subject(:context) { CreateDnsWithHostname.call(params) }
-      it 'fails'
-      it 'dont save dns'
-      it 'dont save hostnames'
+      let(:params) { { IP: '1.1.1.1', hostnames: ['lorem.com', 'lorem.com', 'amet.com'] } }
+      subject(:context) { CreateDnsWithHostnames.call(params) }
+      it 'fails' do
+        expect(context).to be_a_failure
+      end
+      it 'dont save dns' do
+        expect { context }.to_not change(Dns.all, :count)
+      end
+      it 'dont save hostnames' do
+        expect { context }.to_not change(Hostname.all, :count)
+      end
     end
   end
 end
