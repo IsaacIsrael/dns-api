db11c84b123aa6e37f3157c3e34758c322ec9057
isaac israel <email.iisrael@gmail.com>
2019-06-11 23:25:53 -0300

    Define API/V1/DNS POST test cases

--- spec/requests/api_v1_dns_spec.rb
+++ spec/requests/api_v1_dns_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "ApiV1Dns", type: :request do
-  describe "GET /api_v1_dns" do
-    it "works! (now write some real specs)" do
-      get api_v1_dns_index_path
-      expect(response).to have_http_status(200)
+  describe 'POST /api_v1_dns' do
+    context 'when given valid params' do
+      it 'have http status 201'
+      it 'return dns created id'
+      it 'create a dns'
+      it 'create hostnames'
+    end
+    context 'when  not given dns' do
+      it 'have http status 400'
+      it 'return error'
+      it 'not create a dns'
+      it 'not create hostnames'
+    end
+    context 'when given invalid hostnames' do
+      it 'have http status 400'
+      it 'return error'
+      it 'not create a dns'
+      it 'not create hostnames'
     end
   end
 end
