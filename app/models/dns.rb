2518cc3092fcbfec3c9442f3105333938f408d5f
isaac israel <email.iisrael@gmail.com>
2019-06-11 22:02:04 -0300

    Validate test it is invalid if is not IPv4 format

--- app/models/dns.rb
+++ app/models/dns.rb
@@ -1,3 +1,9 @@
+require 'resolv'
+
+# DNS model
 class Dns < ApplicationRecord
-  validates :IP, presence: true, uniqueness: true
+  validates :IP,
+            presence: true,
+            uniqueness: true,
+            format: { with: Resolv::IPv4::Regex, message: 'is not IPv4 format' }
 end
