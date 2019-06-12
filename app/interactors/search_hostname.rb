086b7ab507817ee661474e965cb5cd900d9e6890
isaac israel <email.iisrael@gmail.com>
2019-06-12 10:23:18 -0300

    Validate test when given included filter

--- app/interactors/search_hostname.rb
+++ app/interactors/search_hostname.rb
@@ -5,6 +5,7 @@ class SearchHostname
      context.hostnames = Hostname.joins(:dns)
                         .where('dns.id IN (?)', context.dns.select('id'))
                         .group('hostnames.id, name')
+                        .where.not(name: context.filter&.[](:included))
                         .select('hostnames.id ,name , COUNT(name) as match')
   end
 end
