a1852d8f1be4817c8006d75a2c2ac24d18d93d52
isaac israel <email.iisrael@gmail.com>
2019-06-12 10:35:10 -0300

    Create FilterDnsHostnamesPerPage interactor test

--- spec/interactors/filter_dns_hostnames_per_page_spec.rb
+++ spec/interactors/filter_dns_hostnames_per_page_spec.rb
@@ -11,29 +11,57 @@ RSpec.describe FilterDnsHostnamesPerPage, type: :interactor do
     context 'when without params' do
       let(:dns) { SearchDns.call.dns }
       let(:hostnames) { SearchHostname.call(dns: dns).hostnames }
-      subject(:context) {SearchDnsWithHostnamePerPage.call(page: 2, limit: 1, dns: dns, hostnames: hostnames) }
+      subject(:context) {FilterDnsHostnamesPerPage.call(page: 2, limit: 1, dns: dns, hostnames: hostnames) }
 
-      it 'succeeds'
-      it 'return total'
-      it 'return filtered dns'
-      it 'not return dns'
-      it 'return filtered hostnames'
-      it 'not return filtered hostnames'
-      it 'return number of matching dns records'
+      it 'succeeds' do
+        expect(context).to be_a_success
+      end
+      it 'return total' do
+        expect(context.total).to be_kind_of(Numeric)
+      end
+      it 'return filtered dns' do
+        expect(context.dns).to include(dns2.dns)
+      end
+      it 'not return dns' do
+        expect(context.dns).to_not include(dns1.dns, dns3.dns, dns4.dns, dns5.dns)
+      end
+      it 'return filtered hostnames' do
+        expect(context.hostnames.map(&:name)).to include('ipsum.com')
+      end
+      it 'not return filtered hostnames' do
+        expect(context.hostnames.map(&:name)).to_not include('lorem.com', 'sit.com', 'dolor.com', 'amet.com')
+      end
+      it 'return number of matching dns records' do
+        expect(context.hostnames.map(&:match).sort).to eq [4]
+      end
     end
     context 'when given filter' do
       let(:filter) { { included: ['ipsum.com', 'dolor.com'], excluded: ['sit.com'] }}
       let(:dns) { SearchDns.call(filter: filter).dns }
       let(:hostnames) { SearchHostname.call(dns: dns,filter: filter).hostnames }
-      subject(:context) {SearchDnsWithHostnamePerPage.call(page: 2, limit: 1, dns: dns, hostnames: hostnames) }
+      subject(:context) {FilterDnsHostnamesPerPage.call(page: 2, limit: 1, dns: dns, hostnames: hostnames) }
 
-      it 'succeeds'
-      it 'return total'
-      it 'return filtered dns'
-      it 'not return dns'
-      it 'return filtered hostnames'
-      it 'not return filtered hostnames'
-      it 'return number of matching dns records'
+      it 'succeeds' do
+        expect(context).to be_a_success
+      end
+      it 'return total' do
+        expect(context.total).to be_kind_of(Numeric)
+      end
+      it 'return filtered dns' do
+        expect(context.dns).to include(dns3.dns)
+      end
+      it 'not return dns' do
+        expect(context.dns).to_not include(dns1.dns, dns2.dns, dns4.dns, dns5.dns)
+      end
+      it 'return filtered hostnames' do
+        expect(context.hostnames.map(&:name)).to include('amet.com')
+      end
+      it 'not return filtered hostnames' do
+        expect(context.hostnames.map(&:name)).to_not include('ipsum.com', 'dolor.com', 'sit.com', 'lorem.com')
+      end
+      it 'return number of matching dns records' do
+        expect(context.hostnames.map(&:match).sort).to eq [2]
+      end
     end
   end
 end
