28588969341681cfa54202bf3c5cd0cdde9fb9b4
isaac israel <email.iisrael@gmail.com>
2019-06-12 09:08:41 -0300

    Run rails generate interactor search_dns

---
+++ app/interactors/search_dns.rb
@@ -0,0 +1,7 @@
+class SearchDns
+  include Interactor
+
+  def call
+    # TODO
+  end
+end
---
+++ spec/interactors/search_dns_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe SearchDns, type: :interactor do
+  describe '.call' do
+  end
+end
